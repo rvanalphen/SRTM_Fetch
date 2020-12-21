3bf314f6c96715e04c5112cbcfef1dedc6f3ec17
Robert <robertgvanalphen@gmail.com>
2020-12-21 14:48:48 -0500

    added output div

--- public/demCall.js
+++ public/demCall.js
@@ -18,14 +18,16 @@ button.addEventListener('click', async event => {
     // console.log(json);
 });
 
-
 button.addEventListener('click', async event => {
     const lat = document.getElementById('lat').value;
     const lon = document.getElementById('lon').value;
     const source = document.getElementById('source').value;
     const api_url = `elevation/${lat},${lon},${source}`
     const response = await fetch(api_url);
-    const json = await response.json();
-    console.log(json);
-
+    const data = await response.json();
+    console.log(data);
+    const elevation = data.geoPoints[0].elevation;
+    const div = document.getElementById('results')
+    const elevationHTMLString = `<p>The Elevation at ${lat}N, ${lon}W is ${elevation}m</p>`
+    div.innerHTML = elevationHTMLString;
 });
